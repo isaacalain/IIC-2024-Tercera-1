6ddc5030a02da693e22f38afa87fbc16b0ecaf5e
Isaac <isaacalain5@gmail.com>
2024-06-12 21:44:06 -0600

    Primera clase

---
+++ parte_superior.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="Englsih">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Poyecto de Tercera 1" content="Página de prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+
+
+</head>
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+        <img src="./images/menu.png" alt="">
+        </a>
+
+        <p class="texto-menu-izquierda">
+            La casa de Bob
+
+        </p>
+
+
+    </div>
+</menu>
